74057e2014d176982b66b4d2fee25478ae8ac939
Yumiko <wakinghira73@gmail.com>
2021-05-16 20:37:42 +0900

    NOTNULL制約

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_16_045729) do
+ActiveRecord::Schema.define(version: 2021_05_16_112019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.text "detail"
+    t.string "name", null: false
+    t.string "detail", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
