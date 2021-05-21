0648486b8299e361e0abef8bb08dce71512110a0
Yumiko <wakinghira73@gmail.com>
2021-05-21 14:47:02 +0900

    終了期限のマイグレーションファイル修正

--- db/migrate/20210519011918_add_deadline_to_task.rb
+++ db/migrate/20210519011918_add_deadline_to_task.rb
@@ -1,7 +1,7 @@
 class AddDeadlineToTask < ActiveRecord::Migration[5.2]
   def change
     add_column :tasks, :deadline, :date
-      Task.update_all(deadline: Date.today)
-      change_column_null :tasks, :deadline, false
+    Task.update_all(deadline: Date.today)
+    change_column_null :tasks, :deadline, false
   end
 end
