bea2acbbb46870967db6a900bb86d718cfea2563
Yumiko <wakinghira73@gmail.com>
2021-05-25 12:39:19 +0900

    ユーザー名を表示

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     resources :users, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
     end
   end
-    root to: 'sessions#new'
+  root to: 'tasks#index'
 end
